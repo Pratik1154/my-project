85eb70a2c32244066e73d106d8a886feffd7e423
Pratik1154 <pratikofc1154@gmail.com>
2025-12-23 11:53:14 +0530

    test.py

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print('hello')
+class Student:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+    def show(self):
+        print(f"Student name is {self.name} and age of the student is {self.age}")
+
+s1 = Student('Dumraliya Pratik',20)
+s1.show()
+
